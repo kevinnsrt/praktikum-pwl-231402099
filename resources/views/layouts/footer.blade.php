251e597188aa22ab0da1b5da5176fa585483b0e4
kevinnsrt <siraitkevin25@gmail.com>
2024-03-10 21:40:00 +0700

    tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-5 bg-base-300 text-base-content mt-52">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Kevin Namalo Sirait</p>
+    </aside>
+  </footer>
